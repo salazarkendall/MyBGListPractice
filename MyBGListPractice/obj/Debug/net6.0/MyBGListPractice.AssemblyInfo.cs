36fb29068bc24ca206bcfd5188e35b02db7f6711
salazarkendall <kendallsalazarvargas@gmail.com>
2025-02-19 20:45:09 -0600

    added: dummy projects

--- MyBGListPractice/obj/Debug/net6.0/MyBGListPractice.AssemblyInfo.cs
+++ MyBGListPractice/obj/Debug/net6.0/MyBGListPractice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBGListPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d6be489e28423c775ef69f7390b80502fea2da1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6e860623b079e91874a6851dd06b873356a6efb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBGListPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBGListPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
